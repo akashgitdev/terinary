ed3e039c41a207b2d294c9cb3f234801c2d0326e
akashgitdev <nagaakashduddupudi1712@email.com>
2024-10-29 16:24:22 +0530

    commit

---
+++ ter.js
@@ -0,0 +1,17 @@
+// var score = 20;
+// score >= 90 ?console.log("excellent"):score>=60&& score<=89?console.log("good"): console.log("need to improve")
+
+// var day="tuesday";
+// day=="sunday"||day=="saturday"?console.log("weekend"):day=="monday"||"tuesday"||"wednesday"||"thursday"||"friday"?console.log("weekday"):console.log("invalid")
+
+
+// var str="malayalam";
+// for(i in str)
+// var string=str==str.split('').reverse().join('')
+// string?console.log("palindrome"):console.log("not a palindrome")
+
+
+var str="hello";
+for(i=0;i<=str.length;i++){
+    console.log(str.slice(0,i))
+}
